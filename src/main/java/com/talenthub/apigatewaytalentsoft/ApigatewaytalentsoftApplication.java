cb5a7fcdee8687d8db4fccfea4d817d53857cfb7
Wilermo <william.dmo.sec@gmail.com>
2024-05-11 18:05:30 -0500

    Project Upload

---
+++ src/main/java/com/talenthub/apigatewaytalentsoft/ApigatewaytalentsoftApplication.java
@@ -0,0 +1,13 @@
+package com.talenthub.apigatewaytalentsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApigatewaytalentsoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigatewaytalentsoftApplication.class, args);
+	}
+
+}
